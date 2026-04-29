c42b0cc56a59c87c2b5ca3c5c2def428831619f3
Alexis Henríquez <alexis.henriquezb@gmail.com>
2026-04-29 03:16:47 -0400

    Added WebUI project as ASP.NET Core MVC and added post-build events to driven adapters

--- src/BlueZoneNet.Adapter.ForParkingCars.WebUI/Program.cs
+++ src/BlueZoneNet.Adapter.ForParkingCars.WebUI/Program.cs
@@ -1,6 +1,34 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Scan(scan => scan
+    .FromAssemblies(AppDomain.CurrentDomain.GetAssemblies()) // Scans all loaded assemblies
+    .AddClasses(classes => classes.InNamespaces("BlueZoneNet.Adapter.ForObtainingRates.Stub"))
+    .AddClasses(classes => classes.InNamespaces("BlueZoneNet.Adapter.ForPaying.Spy"))
+    .AddClasses(classes => classes.InNamespaces("BlueZoneNet.Adapter.ForStoringTickets.Fake"))
+    .AsImplementedInterfaces()
+    .WithScopedLifetime());
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+app.MapRazorPages()
+   .WithStaticAssets();
 
 app.Run();
